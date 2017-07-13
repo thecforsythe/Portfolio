aa6f2491464930df3c1e9d80813d1c14a852c123
Chuck Forsythe <thecforsythe@gmail.com>
2017-07-13 06:53:51 -0500

    dissabled clickable hamburger, created tabs that reveal one section at a time

--- script.js
+++ script.js
@@ -10,12 +10,34 @@ function Portfolio (image, name, link, date, notes) {
 
 var aboutMe = new Portfolio('aboutMeScreeShot.png', 'About Me', 'https://thecforsythe.github.io/About-Me/', '6-12-17', 'Created first website using HTML and Javascript. Hey everbody has to start somewhere.');
 
+/* Click Hamburger to reveal tabs */
 
+/*
 $(document).ready(function(){
   $('.main-nav').click(function(){
     $('li').slideToggle();
   });
 });
+*/
 
+/* Click tab to reveal div */
 
-console.log(aboutMe)
+$(document).ready(function(){
+  $('.tabs').on('click',function(){
+    var selectedTab = $(this).data('content');
+    $('.tab-content').hide();
+    var makeIDforTab = '#' + selectedTab;
+    $(makeIDforTab).show();
+  });
+});
+/*
+hamburgerReveal = function() {
+  $('.main-nav').click(function(){
+    $('li').slideToggle();
+  });
+};
+
+$(document).ready(function(){
+  hamburgerReveal();
+})*/
+console.log(aboutMe);
