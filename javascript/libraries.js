28d825786d41123126520fd00f270a31b6b6c1b0
Chuck Forsythe <thecforsythe@gmail.com>
2017-07-14 00:04:31 -0500

    establised rem 10px font scale and tweek typography on web design tab

--- javascript/libraries.js
+++ javascript/libraries.js
@@ -4,23 +4,23 @@ var webDesignLibrary = [
 
   {
     screenShotFileName:   'pictures/checkInScreenShot.png',
-    title:                'Check In: A better way to select hotel rooms',
+    title:                '<p class="pNoPadding">Check In: A Better Way to Travel</p>',
     url:                'https://dustinyschild.github.io/hotel-final-project/index.html',
-    date:                 '6-30-2017',
+    date:                 '<p class="pNoPadding">6-30-2017</p>',
     description:          '<p>Working with two other partners, Dustin Yelland and Benjamin Beeksma, we created an API that allows travelers to choose a hotel room in a similar mannor to choosing a seat on airplane. Rooms can be filtered by room type and amenities. I provided a custom layout, css styling and responsive design.</p>',
   },
   {
     screenShotFileName:   'pictures/busMallScreenShot.png',
-    title:                'Bus Mall',
+    title:                '<p class="pNoPadding">Bus Mall</p>',
     url:                  'https://thecforsythe.github.io/busMall/',
-    date:                 '6-23-2017',
+    date:                 '<p class="pNoPadding">6-23-2017</p>',
     description:          '<p>This site features css keyframe animations and a chart powered by chart.js. Click on the pictures to see the animated transitions.</p>',
   },
   {
     screenShotFileName:   'pictures/aboutMeScreenShot.png',
-    title:                'About Me',
+    title:                '<p class="pNoPadding">About Me</p>',
     url:                  'https://thecforsythe.github.io/About-Me/',
-    date:                 '6-9-2017',
+    date:                 '<p class="pNoPadding">6-9-2017</p>',
     description:          '<p>This is my first webpage. Pretty basic, but hey, everyone has to start somewhere!</p>',
   },
 
